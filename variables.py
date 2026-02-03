dc9677d999a366f887c295bcc4f493d146634e4a
Achyuth-Chandanapu <chandanapuachyuth@gmail.com>
2026-02-03 11:38:39 +0530

    added day_2 files

---
+++ variables.py
@@ -0,0 +1,30 @@
+# Valid variable names
+
+a = 10                  
+number = 20             
+total_sum = 30          
+_total = 40             
+value1 = 50             
+myValue = 60            
+MY_VALUE = 70            
+
+print(a)
+print(number)
+print(total_sum)
+print(_total)
+print(value1)
+print(myValue)
+print(MY_VALUE)
+
+# Multiple assignment (different names, same value)
+x = y = z = 100
+print(x, y, z)
+
+# Different names for same type of data
+student_name = "Achyuth"
+studentAge = 21
+STUDENT_MARKS = 85.5
+
+print(student_name)
+print(studentAge)
+print(STUDENT_MARKS)
